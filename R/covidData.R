05f41b75b79fe5356592be11c14001da78630b4c
prigrecov <pgre0007@student.monash.edu>
2020-10-18 15:19:22 +1100

    Uploaded the raw dataset.

---
+++ R/covidData.R
@@ -0,0 +1,18 @@
+#' Our World in Data's Covid Data
+#'
+#' A dataset containing crop yields for countries over time. Yields   TODO
+#' are measured in tonnes per hectare for seven crops.                TODO
+#'
+#' @format a tibble with 143825 observations and 5 variables:
+#'
+#' - **xxxx**: full country name
+#' - **yyyy**: three letter country code
+#' - **zzzz**: year from 1961 to 2018
+#' - **wwww**: one of bananas, barley, beans, cassava, cocoa, maize, peas, potatoes, rice, soybeans, wheat
+#' - **bbbb**: the total yield of the crop, measured in tonnes per hectare
+#'
+#'
+#'@import tibble
+#'
+#'@source [Our World in Data](https://github.com/owid/covid-19-data/tree/master/public/data)  
+"covidData"
